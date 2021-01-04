25b35a1c72c360e372c55165830aeaafa24fdafc
Mercury <Kansamas@gmail.com>
2021-01-04 05:43:09 +0200

    Obj can now be placed

---
+++ Jarton/Assets/Scripts/ARPlaceObj.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ARPlaceObj : MonoBehaviour
+{
+
+    public GameObject gameobjectToInstance;
+
+    GameObject spawnedObj;
+    ARRaycastManager _arRaycastManager;
+    Vector2 touchPosition;
+
+    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+
+    private void Awake()
+    {
+        _arRaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool tryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+
+        touchPosition = default;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!tryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+        if(_arRaycastManager.Raycast(touchPosition, hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = hits[0].pose;
+
+            if(spawnedObj == null)
+            {
+                spawnedObj = Instantiate(gameobjectToInstance, hitPose.position, hitPose.rotation);
+            }
+            else
+            {
+                spawnedObj.transform.position = hitPose.position;
+            }
+        }
+    }
+}
